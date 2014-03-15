027ef3b838b17f8b708c3d75db3281cdf55a90d4
Matt King <mking@mking.me>
2014-03-14 17:33:20 -0700

    move build completely to grunt, use grunt-requirejs instead of grunt-contrib-requirejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 var sys = require('sys');
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			js: {
 				files: [
@@ -12,106 +12,111 @@ module.exports = function(grunt) {
 				options: { nospawn: true }
 			}
 		},
-    qunit:{
-      target: {
-        src: ['test/**/*.html']
-      },
-	    options: {
-		    '--web-security' : false,
-		    '--local-to-remote-url-access' : true,
-		    '--ignore-ssl-errors' : true
-	    }
-    },
+		qunit:{
+			target: {
+				src: ['test/**/*.html']
+			},
+			options: {
+				'--web-security' : false,
+				'--local-to-remote-url-access' : true,
+				'--ignore-ssl-errors' : true
+			}
+		},
 		requirejs: {
 			compile: {
 				options: {
-					name: "net",
+					almond: true,
+					baseUrl: "src",
 					optimize: "uglify",
-					mainConfigFile: "./build/config.js",
-					out: "./dist/net.js"
+					out: "./dist/net.js",
+					include: ["net"],
+					wrap: {
+						startFile: ["./build/start.frag", "./build/license.frag"],
+						endFile: "./build/end.frag"
+					}
 				}
 			}
 		},
-    // configure jshint task
-    jshint: {
-      options: {
-	      asi: false,
-	      bitwise: false,
-	      boss: false,
-	      browser: true,
-	      couch: false,
-	      curly: true,
-	      debug: false,
-	      devel: false,
-	      eqeqeq: true,
-	      eqnull: false,
-	      evil: false,
-	      expr: false,
-	      forin: false,
-	      globalstrict: true,
-	      globals: { "define": true },
-	      immed: true,
-	      jquery: true,
-	      latedef: true,
-	      laxbreak: false,
-	      loopfunc: false,
-	      mootools: false,
-	      newcap: false,
-	      noarg: true,
-	      node: false,
-	      noempty: false,
-	      nonew: true,
-	      nonstandard: true,
-	      nomen: false,
-	      onevar: false,
-	      passfail: false,
-	      plusplus: false,
-	      prototypejs: false,
-	      regexdash: true,
-	      regexp: false,
-	      rhino: false,
-	      undef: true,
-	      shadow: true,
-	      strict: false,
-	      sub: true,
-	      supernew: false,
-	      trailing: true,
-	      white: false,
-	      wsh: false,
-	      indent: 2,
-	      smarttabs: true
-      },
-      target: {
-        src: ['src/**/*.js']
-      }
-    },
-	  connect: {
-		  server: {
-			  options: {
-				  port: 8000,
-				  base: './test',
-				  middleware: function(connect, options, middlewares) {
-					  middlewares.push(function(req, res, next) {
-						  if (req.url === '/test-post' && req.method === 'POST') {
-							  res.setHeader('Content-Type', 'application/json');
-							  res.end(JSON.stringify({abc: 123}));
-						  } else {
-							  return next();
-						  }
-					  });
-					  return middlewares;
-				  }
-			  }
-		  }
-	  }
-  });
- 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+		// configure jshint task
+		jshint: {
+			options: {
+				asi: false,
+				bitwise: false,
+				boss: false,
+				browser: true,
+				couch: false,
+				curly: true,
+				debug: false,
+				devel: false,
+				eqeqeq: true,
+				eqnull: false,
+				evil: false,
+				expr: false,
+				forin: false,
+				globalstrict: true,
+				globals: { "define": true },
+				immed: true,
+				jquery: true,
+				latedef: true,
+				laxbreak: false,
+				loopfunc: false,
+				mootools: false,
+				newcap: false,
+				noarg: true,
+				node: false,
+				noempty: false,
+				nonew: true,
+				nonstandard: true,
+				nomen: false,
+				onevar: false,
+				passfail: false,
+				plusplus: false,
+				prototypejs: false,
+				regexdash: true,
+				regexp: false,
+				rhino: false,
+				undef: true,
+				shadow: true,
+				strict: false,
+				sub: true,
+				supernew: false,
+				trailing: true,
+				white: false,
+				wsh: false,
+				indent: 2,
+				smarttabs: true
+			},
+			target: {
+				src: ['src/**/*.js']
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 8000,
+					base: './test',
+					middleware: function(connect, options, middlewares) {
+						middlewares.push(function(req, res, next) {
+							if (req.url === '/test-post' && req.method === 'POST') {
+								res.setHeader('Content-Type', 'application/json');
+								res.end(JSON.stringify({abc: 123}));
+							} else {
+								return next();
+							}
+						});
+						return middlewares;
+					}
+				}
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
- 
+	
 	grunt.registerTask('build', ['jshint', 'requirejs:compile', 'qunit']);
-  grunt.registerTask('default', ['connect', 'watch']);
+	grunt.registerTask('default', ['connect', 'watch']);
 };
